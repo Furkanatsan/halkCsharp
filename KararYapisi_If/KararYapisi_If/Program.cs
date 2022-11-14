acc492d3e5314f62c9b4bec60a1d5ccb7bf2b24b
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-14 14:49:01 +0300

    Basit sayı karşılaştırma

---
+++ KararYapisi_If/KararYapisi_If/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random rastgeleSayiUretici = new Random();
+int uretilenSayi = rastgeleSayiUretici.Next(1, 100);
+Console.WriteLine("Tutulan sayıyı tahmin edin");
+var tahmin = int.Parse(Console.ReadLine());
+if (uretilenSayi > tahmin)
+{
+    Console.WriteLine($"üretilen sayı, {tahmin} sayısından büyük....");
+}
+else if (uretilenSayi < tahmin)
+{
+    Console.WriteLine($"üretilen sayı, {tahmin} sayısından küçük....");
+
+}
+else
+{
+    Console.WriteLine("Bildiniz!");
+}
+
