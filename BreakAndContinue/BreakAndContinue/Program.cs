30537f51265cf9fba88911f2f0062d7a7fb755c8
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-15 10:26:42 +0300

    discussion about loops

---
+++ BreakAndContinue/BreakAndContinue/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace BreakAndContinue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "klavye", "gitar", "armut", "webex", "yazılım" };
+            //amaç: kullanıcının girdiği kelime; words içerisinde var mı?
+            Console.WriteLine("Aranacak kelimeyi girin");
+            string searchingWord = Console.ReadLine();
+            bool isWordFinded = false;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (searchingWord == words[i])
+                {
+                    Console.WriteLine($"{searchingWord} array içinde var.");
+                    isWordFinded = true;
+                    break;
+                }
+            }
+
+
+            if (isWordFinded)
+            {
+                Console.WriteLine($"{searchingWord} kelimesi bulunamadı...");
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length != 5)
+                {
+                    continue;
+                }
+                Console.WriteLine(words[i]);
+            }
+
+
+
+        }
+    }
+}
---
+++ LoopsAndArrays/LoopsAndArrays/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LoopsAndArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 42, 60, 90, 0, -6, 150, 4 };
+
+
+            //Minimum değeri bul:
+
+            var minimum = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (minimum > numbers[i])
+                {
+                    minimum = numbers[i];
+                }
+            }
+
+            Console.WriteLine(minimum);
+
+            //Ortalama al:
+            var total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                total += numbers[i];
+            }
+            Console.WriteLine($"Toplam: {total}");
+            var avg = total / numbers.Length;
+            Console.WriteLine($"Ortalama: {avg}");
+        }
+
+
+    }
+}
