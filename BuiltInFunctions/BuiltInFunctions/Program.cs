11617a3e088238781579042e54ad4210c297eee2
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-15 12:01:39 +0300

    Built in functions explained

---
+++ BuiltInFunctions/BuiltInFunctions/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace BuiltInFunctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kelime = "halkbank";
+            Console.WriteLine("Aranacak harf");
+            var aranacakKelime = Console.ReadLine();
+            bool harfVarMi = kelime.Contains(aranacakKelime);
+            if (harfVarMi)
+            {
+                Console.WriteLine($"{kelime.ToUpper()} içinde {aranacakKelime.ToUpper()} ifadesi bulunuyor...");
+            }
+
+            char aranacakHarf = Convert.ToChar(Console.ReadLine());
+            int index = kelime.IndexOf(aranacakHarf);
+
+            //Console.WriteLine($"{kelime} kelimesinde {aranacakHarf} harfinin indexi {index}...");
+            int baslangicNoktasi = 0;
+            int bulunanIndex = 0;
+            while (kelime.IndexOf(aranacakHarf, baslangicNoktasi) != -1)
+            {
+                bulunanIndex = kelime.IndexOf(aranacakHarf, baslangicNoktasi);
+                baslangicNoktasi = bulunanIndex + 1;
+                Console.WriteLine($"{aranacakHarf}, {kelime} kelimesinin {bulunanIndex}. indexinde");
+            }
+
+            int sayi = 5;
+            Console.WriteLine(Convert.ToString(sayi--));
+            Console.WriteLine(sayi);
+
+        }
+    }
+}
