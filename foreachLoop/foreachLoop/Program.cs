5011b820942cbf38a5d9b23f18e43198c43035b6
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-15 10:58:36 +0300

    mini foreach example

---
+++ foreachLoop/foreachLoop/Program.cs
@@ -0,0 +1,11 @@
+﻿var countries = new string[] { "Türkiye", "Norveç", "Almanya", "Kanada" };
+foreach (var country in countries)
+{
+
+	if (country == "Almanya")
+	{
+		break;
+	}
+	Console.WriteLine(country);
+}
+
