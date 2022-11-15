027435c68f644f6da44b3930bab2e365fc24237a
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-15 12:17:54 +0300

    Introduce Functions

---
+++ Functions/Functions/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ekranaYazdir("Bir sayı girin");
+            int sayi = girilenSayiyiOku();
+            if (ciftMi(sayi))
+            {
+                ekranaYazdir($"{sayi} sayısı çifttir");
+            }
+            else
+            {
+                ekranaYazdir($"{sayi} sayısı tektir");
+            }
+        }
+
+        static void ekranaYazdir(string message)
+        {
+            Console.WriteLine(message);
+
+        }
+
+        static int girilenSayiyiOku()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+
+        static bool ciftMi(int sayi)
+        {
+            return sayi % 2 == 0;
+        }
+    }
+
+
+}
