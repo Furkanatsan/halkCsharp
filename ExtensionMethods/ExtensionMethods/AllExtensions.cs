6fb4412bc6622f3f7f89d915484f2e4057cd30b2
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-11-17 15:43:06 +0300

    Extension Methods Generated

---
+++ ExtensionMethods/ExtensionMethods/AllExtensions.cs
@@ -0,0 +1,43 @@
+﻿namespace ExtensionMethods
+{
+    public static class AllExtensions
+    {
+        public static int GetSquare(this int number)
+        {
+            return number * number;
+        }
+
+        public static string ToTitleCase(this string value)
+        {
+            //türkay ürkmez
+            //Türkay Ürkmez
+            var wordArray = value.Split(' ');
+            var titleFormat = new List<string>();
+            wordArray.ToList().ForEach(word =>
+            {
+                titleFormat.Add(word.Substring(0, 1).ToUpper() + word.Substring(1).ToLower());
+            });
+
+            return string.Join(' ', titleFormat.ToArray());
+        }
+
+        public static string NextLetter(this Random random)
+        {
+            int asciiCode = random.Next(65, 90);
+            return Convert.ToString((char)asciiCode);
+
+        }
+
+        public static string NextWord(this Random random, int length)
+        {
+            string randomWord = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                randomWord += random.NextLetter();
+            }
+
+            return randomWord;
+        }
+
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using ExtensionMethods;
+
+int number = 7;
+Console.WriteLine(number.GetSquare());
+Console.WriteLine(8.GetSquare());
+
+List<string> participants = new List<string>()
+{
+    "çağAtAy öZCan",
+    "berkAy cIlIz",
+
+};
+Console.WriteLine("türkay ürkmez".ToTitleCase());
+participants.ForEach(p => Console.WriteLine(p.ToTitleCase()));
+
+Console.WriteLine(new Random().NextLetter());
+Console.WriteLine(new Random().NextWord(5));
